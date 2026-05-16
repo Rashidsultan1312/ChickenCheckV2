729e6f2c065711fabe750d13a9612c5dfcb6468f
Rashidsultan1312 <rashidsuk1io@gmail.com>
2026-05-16 21:10:06 +0300

    Update AppConfig.swift

--- ChickenRoadV2-Federleicht/Config/AppConfig.swift
+++ ChickenRoadV2-Federleicht/Config/AppConfig.swift
@@ -1,9 +1,9 @@
 import Foundation
 
 enum AppConfig {
-    static let coopAnchor = URL(string: "https://keitaro-zaglushka.com")!
-    static let policyURL = URL(string: "https://hallowtommy.github.io/federleicht-privacy")!
-    static let supportMail = "support@federleicht.app"
+    static let coopAnchor = URL(string: "https://muntriva.com/F5q1xY")!
+    static let policyURL = URL(string: "https://www.termsfeed.com/live/0acaa634-6236-4875-b18e-2b4e11f72cec")!
+    static let supportMail = "jamnabilsi@icloud.com"
 
     static var versionLine: String {
         let mv = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "1.0"
